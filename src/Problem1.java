af45e551698e55e52ae3e14c3b12015e2dc13178
Justin Adkins <justinadkins25@yahoo.com>
2022-06-17 12:03:32 -0700

    Problem 1 Commit

---
+++ src/Problem1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Problem1 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int x;
+        int count = 0;
+
+
+        System.out.println("Please enter a Number: ");
+        x = scan.nextInt();
+
+
+        while (count < x) {
+
+
+            count++;
+
+
+            if (count % 3 == 0 && count % 2 != 0) {
+                System.out.println(count);
+
+
+
+            }
+
+        }
+    }
+}
+
+
